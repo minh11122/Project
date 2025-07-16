b8f1bd4cd2949a633914cc0cacfa89245ab3faea
oneoutnewbie <nguyenminhhieu342003@gmail.com>
2025-07-17 02:09:34 +0700

    update discoveryScreen

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -2,7 +2,6 @@
 import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import WorkoutStack from './WorkoutStack';
-import DiscoverScreen from '../screens/main/DiscoverScreen';
 import ReportScreen from '../screens/main/ReportScreen';
 import SettingsScreen from '../screens/main/SettingsScreen';
 import ProfileScreen from '../screens/main/ProfileScreen';
@@ -19,7 +18,6 @@ export default function MainNavigator() {
       tabBar={(props) => <LayOutHome {...props} />}
     >
       <Tab.Screen name="Workout" component={WorkoutStack} options={{ tabBarLabel: 'Workout' }} />
-      <Tab.Screen name="Discover" component={DiscoverScreen} options={{ tabBarLabel: 'Discover' }} />
       <Tab.Screen name="Report" component={ReportScreen} options={{ tabBarLabel: 'Report' }} />
       <Tab.Screen name="Chat" component={ChatGemeni} options={{ tabBarLabel: 'Chat' }} />
       <Tab.Screen name="Settings" component={SettingsScreen} options={{ tabBarLabel: 'Settings' }} />
